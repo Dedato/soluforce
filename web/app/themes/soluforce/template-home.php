34836dae36d8dbdaf5067fdc6d4304d944283b68
Toine Kamps <toinekamps@gmail.com>
2015-11-30 17:24:45 +0100

    Add acf sections to data

--- web/app/themes/soluforce/template-home.php
+++ web/app/themes/soluforce/template-home.php
@@ -3,6 +3,8 @@
  * Template Name: Homepage
  */
 
-$data             = Timber::get_context();
-$data['post']     = new TimberPost();
+$data              = Timber::get_context();
+$data['post']      = new TimberPost();
+$data['carousel']  = get_field('acf_carousel_slides');
+$data['sections']  = get_field('acf_content_blocks');
 Timber::render('home.twig', $data);
