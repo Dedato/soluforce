c2d164a9f8301a2a353b8196347ca28b437e108d
Toine Kamps <toinekamps@gmail.com>
2015-12-07 17:14:34 +0100

    Add header image data

--- web/app/themes/soluforce/page.php
+++ web/app/themes/soluforce/page.php
@@ -1,4 +1,6 @@
 <?php
-$data            = Timber::get_context();
-$data['post']    = new TimberPost();
+$data                 = Timber::get_context();
+$data['post']         = new TimberPost();
+$data['header_img']   = get_field('acf_header_image');
+$data['sidebar_img']  = get_field('acf_sidebar_image');
 Timber::render('page.twig', $data);
