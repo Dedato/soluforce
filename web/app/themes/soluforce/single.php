dbfd73d32706382185185226e869abd5f269d334
Toine Kamps <toinekamps@gmail.com>
2015-12-08 17:36:36 +0100

    Add data to single solution

--- web/app/themes/soluforce/single.php
+++ web/app/themes/soluforce/single.php
@@ -1,4 +1,30 @@
 <?php
-$data         = Timber::get_context();
-$data['post'] = new TimberPost();
-Timber::render('single.twig', $data);
+$data                       = Timber::get_context();
+$post                       = Timber::query_post();
+$data['post']               = $post;
+
+if (is_singular('solution') && get_field('acf_solution_products')) :
+  // Get connected cases
+	$case_args = array(
+		'post_type'   => 'case',
+		'meta_query'  => array(
+			array(
+				'key'       => 'acf_case_solutions',
+				'value'     => '"' . $post->ID . '"',
+				'compare'   => 'LIKE'
+			)
+		)
+	);
+	$data['solution_cases'] = Timber::get_posts($case_args);
+  // Get connected products
+  $product_ids  = get_field('acf_solution_products', false, false);
+  $product_args = array(
+    'post_type'       => 'product',
+    'post__in'		    => $product_ids,
+    'orderby'         => 'post__in',
+    'posts_per_page'	=> -1
+  );
+  $data['solution_products'] = Timber::get_posts($product_args);
+endif;
+
+Timber::render(array( 'single-' . $post->post_type . '.twig', 'single.twig' ), $data);
