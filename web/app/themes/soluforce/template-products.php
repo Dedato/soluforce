1bc77ef47883945c4c4a50d2a82ce845fcd0b3cb
Toine Kamps <toinekamps@gmail.com>
2015-12-10 17:31:21 +0100

    Add products query

--- web/app/themes/soluforce/template-products.php
+++ web/app/themes/soluforce/template-products.php
@@ -3,6 +3,12 @@
  * Template Name: Products
  */
 
-$data         = Timber::get_context();
-$data['post'] = new TimberPost();
-Timber::render('page.twig', $data);
+$data              = Timber::get_context();
+$data['post']      = new TimberPost();
+$data['products']  = Timber::get_posts(array(
+  'post_type' => 'product',
+  'orderby'   => 'menu_order',
+  'order'     => 'ASC',
+  'nopaging'  => true
+));
+Timber::render('tmpl-products.twig', $data);
