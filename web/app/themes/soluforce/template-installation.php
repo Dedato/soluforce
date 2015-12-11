e339ca96663293e41b5daec05eea9e660226fdda
Toine Kamps <toinekamps@gmail.com>
2015-12-11 17:20:46 +0100

    Add installation query

--- web/app/themes/soluforce/template-installation.php
+++ web/app/themes/soluforce/template-installation.php
@@ -3,6 +3,14 @@
  * Template Name: Installation
  */
 
-$data         = Timber::get_context();
-$data['post'] = new TimberPost();
-Timber::render('page.twig', $data);
+$data                   = Timber::get_context();
+$data['post']           = new TimberPost();
+$data['header_img']     = get_field('acf_header_image');
+$data['installations']  = Timber::get_posts(array(
+  'post_type' => 'installation',
+  'orderby'   => 'menu_order',
+  'order'     => 'ASC',
+  'nopaging'  => true
+));
+
+Timber::render('tmpl-installation.twig', $data);
