eb606b4bdb7b12006f1a3da2298c608a74c451a5
Toine Kamps <toinekamps@gmail.com>
2015-12-15 17:22:13 +0100

    Add solutions with products

--- web/app/themes/soluforce/template-products.php
+++ web/app/themes/soluforce/template-products.php
@@ -3,12 +3,25 @@
  * Template Name: Products
  */
 
-$data              = Timber::get_context();
-$data['post']      = new TimberPost();
-$data['products']  = Timber::get_posts(array(
-  'post_type' => 'product',
-  'orderby'   => 'menu_order',
-  'order'     => 'ASC',
-  'nopaging'  => true
+$data                       = Timber::get_context();
+$data['post']               = new TimberPost();
+$data['products']           = Timber::get_posts(array(
+  'post_type'   => 'product',
+  'orderby'     => 'menu_order',
+  'order'       => 'ASC',
+  'nopaging'    => true
 ));
+$data['solutions_products'] = Timber::get_posts(array(
+  'post_type'   => 'solution',
+  'meta_query'	=> array(
+		'relation'	  => 'AND',
+		array(
+			'key'	 	      => 'acf_solution_products',
+		),
+	),
+  'orderby'     => 'menu_order',
+  'order'       => 'ASC',
+  'nopaging'    => true
+));
+
 Timber::render('tmpl-products.twig', $data);
